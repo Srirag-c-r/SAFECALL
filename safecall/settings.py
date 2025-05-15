f72beab0a52fceecd284170c390749870c3d4865
athulroy656 <athulroy656@gmail.com>
2025-05-15 19:13:32 +0530

    second commit

--- safecall/settings.py
+++ safecall/settings.py
@@ -55,6 +55,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'core.middleware.NoCacheMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'safecall.urls'
